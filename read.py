5df6d148e6357c3233626b79f3224465de5e35b3
Fiya Lee <beautifullife1027@hotmail.com>
2020-03-23 17:03:12 +0800

    add average

--- read.py
+++ read.py
@@ -7,8 +7,10 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:
 			print(len(data))
 			# % count 跟1000求餘數為0(1000的倍數),則.. 
-print(len(data))  #每一次只讀取一行
+print('檔案讀取完，總共有', len(data), '筆資料')  #每一次只讀取一行
 
-print(data[0])
-print('----------------------')
-print(data[1])
+sum_len = 0 #目前的總數
+for d in data:
+	sum_len = sum_len + len(d)
+	
+print('每筆留言平均長度是', sum_len/len(data))
